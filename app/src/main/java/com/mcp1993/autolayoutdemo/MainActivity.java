b0b9ce6b73a5dbdf7b937494b39bbe094b91dbff
mcp1993 <1023042656@qq.com>
2016-10-08 15:08:52 +0800

    AutoLayoutTest

---
+++ app/src/main/java/com/mcp1993/autolayoutdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mcp1993.autolayoutdemo;
+
+import android.os.Bundle;
+
+import com.zhy.autolayout.AutoLayoutActivity;
+
+public class MainActivity extends AutoLayoutActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
